dab0b7145810235d4fb61c448c39a49373107d2b
icarodr <it46009@gmail.com>
2022-06-01 16:15:24 -0300

    lembrar de mudar a path do txt

--- src/base/Arquivo.java
+++ src/base/Arquivo.java
@@ -6,13 +6,13 @@ import java.nio.file.Path;
 public class Arquivo {
     public static void main(String[] args) {
 
-        Path caminho = (Path) Paths.get("C:/Users/ct67ca/Desktop/Somativa de Java/Somativa-de-Java-09.06/Usuarios.txt");
+        Path caminho = (Path) Paths.get("Lembrar de trocar o caminho da pasta");
         String texto = "Isso é um teste";
         byte[] textoEmByte = texto.getBytes();
 
         try {
                 Files.write(caminho, textoEmByte);
-
+                
         } catch (Exception erro) {}
     }
 }
